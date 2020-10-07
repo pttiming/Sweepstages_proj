b0fb56b609a627b8dc004d88bddd93a723b8806a
pttiming <sean@pttiming.com>
2020-10-07 08:47:42 -0500

    Initital Classes added

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        //Member Variables
+        string firstName;
+        string lastName;
+        string emailAddress;
+        int registrationNumber;
+        //Constructor
+
+        //Methods
+    }
+}
